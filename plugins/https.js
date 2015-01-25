564844ee533364989f00210738a8250f0173576a
Rasmus Eskola <fruitiex@gmail.com>
2015-01-25 13:42:17 +0200

    wip: https

--- plugins/https.js
+++ plugins/https.js
@@ -1,7 +1,18 @@
 var express = require('express');
+var https = require('https');
+var fs = require('fs');
 
 var expressjs = {};
 var config, player;
+
+var options = {
+    tls: config.tls,
+    key: config.tlsKey,
+    cert: config.tlsCert,
+    ca: config.tlsCa,
+    requestCert: config.requestCert,
+    rejectUnauthorized: config.rejectUnauthorized
+};
 
 // called when partyplay is started to initialize the plugin
 // do any necessary initialization here
@@ -10,7 +21,7 @@ expressjs.init = function(_player, callback, errCallback) {
     config = _player.config;
 
     player.expressApp = express();
-    player.expressServer = player.expressApp.listen(process.env.PORT || config.port);
+    player.expressServer = https.createServer(options, player.expressApp).listen(process.env.PORT || config.port);
 
     callback();
 };
