e990541829508fc7289656a04f67207bd7a821b7
Rasmus Eskola <fruitiex@gmail.com>
2015-02-01 13:41:15 +0200

    https -> http module, if config.tls is true then use https

---
+++ plugins/http.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var https = require('https');
+var http = require('https');
+var fs = require('fs');
+
+var expressjs = {};
+var config, player;
+
+// called when nodeplayer is started to initialize the plugin
+// do any necessary initialization here
+expressjs.init = function(_player, callback, errCallback) {
+    player = _player;
+    config = _player.config;
+
+    player.expressApp = express();
+
+    var options = {};
+    if(config.tls) {
+        options = {
+            tls: config.tls,
+            key: fs.readFileSync(config.tlsKey),
+            cert: fs.readFileSync(config.tlsCert),
+            ca: fs.readFileSync(config.tlsCa),
+            requestCert: config.requestCert,
+            rejectUnauthorized: config.rejectUnauthorized
+        };
+        player.expressTls = true;
+        player.expressServer = https.createServer(options, player.expressApp).listen(process.env.PORT || config.port);
+    } else {
+        player.expressServer = http.createServer(options, player.expressApp).listen(process.env.PORT || config.port);
+    }
+
+    callback();
+};
+
+module.exports = expressjs;
