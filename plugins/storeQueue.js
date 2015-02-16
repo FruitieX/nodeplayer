702859b55fbc5b13e7b5dc908c4c7e1d7cbdf26b
FruitieX <fruitiex@gmail.com>
2015-02-16 13:22:56 +0200

    optional plugin that stores current queue on disk and restores it on program start

---
+++ plugins/storeQueue.js
@@ -0,0 +1,23 @@
+var storeQueue = {};
+var fs = require('fs');
+var mkdirp = require('mkdirp');
+
+storeQueue.path = process.env.HOME + '/.nodeplayer/stored-queue.json';
+
+storeQueue.init = function(_player, callback, errCallback) {
+    player = _player;
+    config = _player.config;
+
+    mkdirp(process.env.HOME + '/.nodeplayer');
+    if(fs.existsSync(storeQueue.path)) {
+        player.queue = JSON.parse(fs.readFileSync(storeQueue.path));
+    }
+
+    callback();
+};
+
+storeQueue.onQueueModify = function() {
+    fs.writeFileSync(storeQueue.path, JSON.stringify(player.queue, undefined, 4));
+};
+
+module.exports = storeQueue;
