2372807ec7bc40f4e29c9572869925dfd7527683
Rasmus Eskola <fruitiex@gmail.com>
2015-03-20 11:06:32 +0200

    refactor index.js into reusable components

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 'use strict';
+
 var _ = require('underscore');
 var npm = require('npm');
 var async = require('async');
@@ -9,22 +10,24 @@ var config = nodeplayerConfig.getConfig();
 
 var logger = labeledLogger('core');
 
-function checkModule(module) {
+function Core() {
+    this.player = new Player();
+}
+
+Core.prototype.checkModule = function(module) {
     try {
         require.resolve(module);
         return true;
     } catch (e) {
         return false;
     }
-}
-
-var player = new Player();
+};
 
 // make sure all modules are installed, installs missing ones, then calls loadCallback
-function installModules(modules, moduleType, loadCallback) {
-    async.eachSeries(modules, function(moduleShortName, callback) {
+Core.prototype.installModules = function(modules, moduleType, loadCallback) {
+    async.eachSeries(modules, _.bind(function(moduleShortName, callback) {
         var moduleName = 'nodeplayer-' + moduleType + '-' + moduleShortName;
-        if (!checkModule(moduleName)) {
+        if (!this.checkModule(moduleName)) {
             logger.info(moduleName + ' module not found, installing...');
             npm.load({}, function(err) {
                 npm.commands.install(__dirname, [moduleName], function(err) {
@@ -41,49 +44,58 @@ function installModules(modules, moduleType, loadCallback) {
             // skip already installed
             callback();
         }
-    }, loadCallback);
-}
+    }, this), loadCallback);
+};
 
-function initModule(moduleShortName, moduleType, callback) {
+Core.prototype.initModule = function(moduleShortName, moduleType, callback) {
     var moduleTypeCapital = moduleType.charAt(0).toUpperCase() + moduleType.slice(1);
     var moduleName = 'nodeplayer-' + moduleType + '-' + moduleShortName;
     var module = require(moduleName);
 
     var moduleLogger = labeledLogger(moduleShortName);
-    module.init(player, moduleLogger, function(err) {
+    module.init(this.player, moduleLogger, _.bind(function(err) {
         if (!err) {
-            player[moduleType + 's'][moduleShortName] = module;
+            this[moduleType + 's'][moduleShortName] = module;
             if (moduleType === 'backend') {
-                player.songsPreparing[moduleShortName] = {};
+                this.songsPreparing[moduleShortName] = {};
             }
 
             moduleLogger.info(moduleType + ' module initialized');
-            player.callHooks('on' + moduleTypeCapital + 'Initialized', [moduleShortName]);
+            this.callHooks('on' + moduleTypeCapital + 'Initialized', [moduleShortName]);
         } else {
             moduleLogger.error('while initializing: ' + err);
-            player.callHooks('on' + moduleTypeCapital + 'InitError', [moduleShortName, err]);
+            this.callHooks('on' + moduleTypeCapital + 'InitError', [moduleShortName, err]);
         }
         callback(err);
-    });
-}
+    }, this.player));
+};
 
-async.eachSeries(['plugin', 'backend'], function(moduleType, callback) {
-    // first install missing modules
-    installModules(config[moduleType + 's'], moduleType, callback);
-}, function() {
-    // then initialize modules, first all plugins in series, then all backends in parallel
-    async.eachSeries(['plugin', 'backend'], function(moduleType, moduleCallback) {
-        var moduleTypeCapital = moduleType.charAt(0).toUpperCase() + moduleType.slice(1);
+Core.prototype.initModules = function(callback) {
+    async.eachSeries(['plugin', 'backend'], _.bind(function(moduleType, installCallback) {
+        // first install missing modules
+        this.installModules(config[moduleType + 's'], moduleType, installCallback);
+    }, this), _.bind(function() {
+        // then initialize modules, first all plugins in series, then all backends in parallel
+        async.eachSeries(['plugin', 'backend'], _.bind(function(moduleType, typeCallback) {
+            var moduleTypeCapital = moduleType.charAt(0).toUpperCase() + moduleType.slice(1);
 
-        (moduleType === 'plugin' ? async.eachSeries : async.each)
-            (config[moduleType + 's'], function(moduleName, callback) {
-            if (checkModule('nodeplayer-' + moduleType + '-' + moduleName)) {
-                initModule(moduleName, moduleType, callback);
-            }
-        }, function(err) {
-            logger.info('all ' + moduleType + ' modules initialized');
-            player.callHooks('on' + moduleTypeCapital + 'sInitialized');
-            moduleCallback();
+            (moduleType === 'plugin' ? async.eachSeries : async.each)
+                (config[moduleType + 's'], _.bind(function(moduleName, moduleCallback) {
+                if (this.checkModule('nodeplayer-' + moduleType + '-' + moduleName)) {
+                    this.initModule(moduleName, moduleType, moduleCallback);
+                }
+            }, this), _.bind(function(err) {
+                logger.info('all ' + moduleType + ' modules initialized');
+                this.callHooks('on' + moduleTypeCapital + 'sInitialized');
+                typeCallback();
+            }, this.player));
+        }, this), function() {
+            callback();
         });
-    });
-});
+    }, this));
+};
+
+Core.prototype.Player = Player;
+Core.prototype.labeledLogger = labeledLogger;
+
+module.exports = Core;
